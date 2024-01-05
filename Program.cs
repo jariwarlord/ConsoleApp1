007eec6ca7d63102c1904878b866baba84afc042
jariwarlord <berkedalar4@gmail.com>
2024-01-05 15:37:44 +0300

    Update

--- Program.cs
+++ Program.cs
@@ -4,15 +4,15 @@ class Program
 {
     static void Main()
     {
-        // Alpha Vantage API Key'i
+        
         string apiKey = "QYOD9Z3W3OAGBGU9";
-        // İlgili hisse senedi simgesi (örneğin: AAPL)
+        
         string stockSymbol = "AAPL";
 
-        // StockApiService sınıfını oluşturun
+        
         StockApiService stockApiService = new StockApiService(apiKey);
 
-        // Dün ve bugünün fiyatlarını al
+        
         var (yesterdayPrice, todayPrice) = stockApiService.GetLastTwoDaysPrices(stockSymbol);
         Console.WriteLine($"Dünden Bugüne Kapanış Fiyatları: Dün: {yesterdayPrice}, Bugün: {todayPrice}");
 
